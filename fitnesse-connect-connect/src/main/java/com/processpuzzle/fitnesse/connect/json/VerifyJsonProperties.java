97f09cc8ac74aa1860d5a310df71c329f399cfed
ZsZs <zsolt.zsuffa@gmail.com>
2017-03-22 06:53:58 +0100

    Correct unit test for property selection.

--- fitnesse-connect-connect/src/main/java/com/processpuzzle/fitnesse/connect/json/VerifyJsonProperties.java
+++ fitnesse-connect-connect/src/main/java/com/processpuzzle/fitnesse/connect/json/VerifyJsonProperties.java
@@ -1,36 +1,44 @@
 package com.processpuzzle.fitnesse.connect.json;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.PathNotFoundException;
+
 public class VerifyJsonProperties {
+   private static final Logger log = LoggerFactory.getLogger(  VerifyJsonProperties.class );
    private static final String PASS_TOKEN = "pass";
    private final String jsonObject;
    private String propertyDescription;
    private boolean propertyMandatory;
-   private String propertyName;
+   private String propertySelector;
    private PropertyDataTypes propertyType;
+   private String propertyValue;
    private boolean propertyValueNotNull;
    private String returnMessage = "pass";
-    
+
    // constructors
-   public VerifyJsonProperties( String jsonObject ){
+   public VerifyJsonProperties( String jsonObject ) {
       this.jsonObject = jsonObject;
    }
-    
+
    // public accessors and mutators
-   public String verifyProperty(){
+   public String verifyProperty() {
       determineCurrentProperty();
       verifyPropertyName();
       return this.returnMessage;
-   } 
-   
+   }
+
    // properties
    // @formatter:off
-   public void setProperty( String propertyName ){ this.propertyName = propertyName; }
-   public void setDataType( String propertyType ){ this.propertyType = PropertyDataTypes.parse( propertyType ); }
-   public void setDescription( String propertyDescription ){ this.propertyDescription = propertyDescription; }
-   public void setIsMandatory( String propertyMandatory ){ this.propertyMandatory = "yes".equals( propertyMandatory ); }
-   public void setNotNull( String propertyValueNotNull ){ this.propertyValueNotNull = "yes".equals( propertyValueNotNull ); }
+   public void setProperty( String propertyName ) { this.propertySelector = propertyName; }
+   public void setDataType( String propertyType ) { this.propertyType = PropertyDataTypes.parse( propertyType ); }
+   public void setDescription( String propertyDescription ) { this.propertyDescription = propertyDescription; }
+   public void setIsMandatory( String propertyMandatory ) { this.propertyMandatory = "yes".equals( propertyMandatory ); }
+   public void setNotNull( String propertyValueNotNull ) { this.propertyValueNotNull = "yes".equals( propertyValueNotNull ); }
    // @formatter:on
-   
+
    // protected, private helper methods
    private void combineReturnMessage( String additionalMessage ) {
       if( additionalMessage.equals( PASS_TOKEN ) ){
@@ -47,18 +55,23 @@ public class VerifyJsonProperties {
    }
 
    private void determineCurrentProperty() {
+      try{
+         this.propertyValue = JsonPath.parse( jsonObject ).read( this.propertySelector ).toString();         
+      }catch( PathNotFoundException e ){
+         log.info( "Selecting property: " + this.propertySelector + " failed." );
+      }
    }
 
    private void verifyPropertyName() {
       String result = PASS_TOKEN;
       if( this.propertyMandatory ){
-         if( this.columnIndex != null ){
+         if( this.propertyValue != null ){
             result = PASS_TOKEN;
          }else{
             result = "failed: property not defined";
          }
       }
-      
+
       combineReturnMessage( result );
    }
 
--- fitnesse-connect-connect/src/test/java/com/processpuzzle/fitnesse/connect/json/VerifyJsonPropertiesTest.java
+++ fitnesse-connect-connect/src/test/java/com/processpuzzle/fitnesse/connect/json/VerifyJsonPropertiesTest.java
@@ -10,55 +10,34 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class VerifyJsonPropertiesTest {
-   private static final String JSON_OBJECT = "{ text: 'Hello World', numeric: 2017, date: '2017/03/21', boolean: yes, optional: 'optional' }";
+   private static final String JSON_OBJECT = "{ 'text': 'Hello World', 'numeric': '2017', 'date': '2017/03/21', 'boolean': 'yes', 'optional': 'optional' }";
    private VerifyJsonProperties verifyJson;
-   
-   @Before public void beforeEachTest() throws Exception{
+
+   @Before public void beforeEachTest() throws Exception {
       verifyJson = new VerifyJsonProperties( JSON_OBJECT );
    }
-   
-   @Test public void verifyProperty_whenPropertyExist_returnsPass(){
-      verifyJson.setProperty( "text" );
+
+   @Test public void verifyProperty_whenPropertyExist_returnsPass() {
+      verifyJson.setProperty( "$['text']" );
       verifyJson.setDataType( "String" );
-      assertThat( verifyJson.verifyProperty(), equalTo( "pass" ));
-   }
-   
-   @Test public void verifyProperty_whenPropertyNotExistAndIsMandatory_returnsFailed(){
-      verifyJson.setProperty( "No property" );
-      verifyJson.setIsMandatory( "yes" );
-      verifyJson.setDataType( "String" );
-      assertThat( verifyJson.verifyProperty(), equalTo( "failed: property not defined" ));
+      assertThat( verifyJson.verifyProperty(), equalTo( "pass" ) );
    }
 
-/*   
-   @Test public void verifyColumn_whenDataTypeMatches_returnsPass(){
-      verifyJson.setProperty( "String Column" );
-      verifyJson.setDataType( "String" );
-      assertThat( verifyJson.verifyProperty(), equalTo( "pass" ));
-      
-      verifyJson.setColumn( "Integer Column" );
-      verifyJson.setDataType( "Integer" );
-      assertThat( verifyJson.verifyProperty(), equalTo( "pass" ));
-      
-      verifyJson.setColumn( "Date Column" );
-      verifyJson.setDataType( "Date" );
-      assertThat( verifyJson.verifyProperty(), equalTo( "pass" ));
-   }
-   
-   @Test public void verifyColumn_whenColumnIsMandatory_returnsPass(){
-      verifyJson.setColumn( "Optional Column" );
-      verifyJson.setDataType( "String" );
-      verifyJson.setIsMandatory( "no" );
-      verifyJson.setIsValueMandatory( "yes" );
-      assertThat( verifyJson.verifyProperty(), equalTo( "pass" ));
-   }
-   
-   @Test public void verifyColumn_whenColumnValueIsMandatory_returnsPass(){
-      verifyJson.setColumn( "Optional Value Column" );
-      verifyJson.setDataType( "String" );
+   @Test public void verifyProperty_whenPropertyNotExistAndIsMandatory_returnsFailed() {
+      verifyJson.setProperty( "$['No property']" );
       verifyJson.setIsMandatory( "yes" );
-      verifyJson.setIsValueMandatory( "no" );
-      assertThat( verifyJson.verifyColumn(), equalTo( "pass" ));
+      verifyJson.setDataType( "String" );
+      assertThat( verifyJson.verifyProperty(), equalTo( "failed: property not defined" ) );
    }
-*/
+
+   /*
+    * @Test public void verifyColumn_whenDataTypeMatches_returnsPass(){ verifyJson.setProperty( "String Column" ); verifyJson.setDataType( "String" );
+    * assertThat( verifyJson.verifyProperty(), equalTo( "pass" )); verifyJson.setColumn( "Integer Column" ); verifyJson.setDataType( "Integer" ); assertThat(
+    * verifyJson.verifyProperty(), equalTo( "pass" )); verifyJson.setColumn( "Date Column" ); verifyJson.setDataType( "Date" ); assertThat(
+    * verifyJson.verifyProperty(), equalTo( "pass" )); }
+    * @Test public void verifyColumn_whenColumnIsMandatory_returnsPass(){ verifyJson.setColumn( "Optional Column" ); verifyJson.setDataType( "String" );
+    * verifyJson.setIsMandatory( "no" ); verifyJson.setIsValueMandatory( "yes" ); assertThat( verifyJson.verifyProperty(), equalTo( "pass" )); }
+    * @Test public void verifyColumn_whenColumnValueIsMandatory_returnsPass(){ verifyJson.setColumn( "Optional Value Column" ); verifyJson.setDataType( "String"
+    * ); verifyJson.setIsMandatory( "yes" ); verifyJson.setIsValueMandatory( "no" ); assertThat( verifyJson.verifyColumn(), equalTo( "pass" )); }
+    */
 }
