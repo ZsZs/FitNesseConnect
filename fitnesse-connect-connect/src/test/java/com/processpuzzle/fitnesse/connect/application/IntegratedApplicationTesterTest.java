b9e6d4e7004fcf8c645fd76dcf4b1fa41ac46e65
ZsZs <zsolt.zsuffa@gmail.com>
2017-05-23 17:14:22 +0200

    Correct IntegratedApplicationTesterTest

--- fitnesse-connect-connect/src/test/java/com/processpuzzle/fitnesse/connect/application/IntegratedApplicationTesterTest.java
+++ fitnesse-connect-connect/src/test/java/com/processpuzzle/fitnesse/connect/application/IntegratedApplicationTesterTest.java
@@ -4,24 +4,28 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith( SpringRunner.class )
 public class IntegratedApplicationTesterTest {
+   private IntegratedApplicationTester applicationTester;
 
-   @Test public void initialize_configuresContext(){
-      IntegratedApplicationTester applicationTester = new IntegratedApplicationTester();
-       applicationTester.initialize( "unit-test" );
-      
-      assertThat( IntegratedApplicationTester.getInstance().getConfiguration( "connector" ).getHost(), equalTo( "127.0.0.1" ));
+   @Before public void beforeEachTests() {
+      applicationTester = new IntegratedApplicationTester();
+      applicationTester.initialize( "unit-test" );
    }
-   
-   @Test public void getInstance_whenNotInstantiatedYet_createsInstance(){
+
+   @Test public void initialize_configuresContext() {
+      assertThat( IntegratedApplicationTester.getInstance().getConfiguration( "connector" ).getHost(), equalTo( "127.0.0.1" ) );
+   }
+
+   @Test public void getInstance_whenNotInstantiatedYet_createsInstance() {
       IntegratedApplicationTester applicationTester = IntegratedApplicationTester.getInstance();
-      
-      assertThat( applicationTester, notNullValue());
+
+      assertThat( applicationTester, notNullValue() );
       assertThat( applicationTester.getApplicationContext(), notNullValue() );
    }
 }
